055b120fe2b4b9087658b4c219b2a181902e23fe
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2024-11-02 22:32:54 +0200

    add adaptive hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,34 +11,89 @@ const Hero = styled.section`
     height: 595px;
     padding: 100px 0;
     font-family: Montserrat;
+    @media (max-width: 1200px){
+        padding: 40px 0;
+    }
+    @media (max-width: 768px){
+        padding: 50px 0 100px;
+        height: 345px;
+    }
 `;
 const TextContainer = styled.div`
     display: flex;
     gap: 52px;
     margin: 80px auto;
     width: 794px;
+    @media (max-width: 1200px){
+        width: 412px;
+        gap: 34px;
+        margin: 40px auto 64px;
+    }
+    @media (max-width: 768px){
+        margin: 35px auto 0;
+        width: 161px;
+        position: absolute;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+        padding-left: 25px;
+        transform: translateX(-50%);
+        left: 50%;
+    }
 `;
 const Title = styled.h1`
     color: #FFF;
     font-size: 40px;
     text-align: center;
     font-weight: 600;
+    @media (max-width: 1200px){
+        font-size: 20px;
+    }
+    @media (max-width: 768px){
+        font-size: 14px;
+        height: 17px;
+    }
 `;
 const Description = styled.h3`
     color: #FFF;
     font-size: 24px;
     width: 345px;
     font-weight: 500;
+    text-align: right;
+    @media (max-width: 1200px){
+        font-size: 14px;
+        width: 172px;
+    }
+    @media (max-width: 768px){
+        font-size: 10px;
+        width: 136px;
+        text-align: left;
+    }
 `;
 const Date = styled.h3`
     color: #FFF;
     font-size: 24px;
     font-weight: 500;
+    @media (max-width: 1200px){
+        font-size: 14px;
+    }
+    @media (max-width: 768px){
+        font-size: 10px;
+    }
 `;
 const Line = styled.div`
     width: 3px;
     height: 144px;
     background: #FFF;
+    @media (max-width: 1200px){
+        height: 110px;
+    }
+    @media (max-width: 768px){
+        position: absolute;
+        left: 0;
+        width: 2px;
+        height: 80px;
+    }
 `;
 const SearchContainer = styled.div`
     width: 625px;
@@ -46,6 +101,15 @@ const SearchContainer = styled.div`
     border-radius: 10px;
     position: relative;
     margin: 0 auto;
+    @media (max-width: 1200px){
+        width: 402px;
+        height: 27px;
+    }
+    @media (max-width: 768px){
+        width: 174px;
+        height: 15px;
+        margin-top: 163px;
+    }
 `;
 const Search = styled.input`
     border: 0;
@@ -58,6 +122,19 @@ const Search = styled.input`
     color: #878787;
     font-size: 14px;
     font-weight: 500;
+    @media (max-width: 1200px){
+        font-size: 10px;
+        width: 402px;
+        height: 27px;
+        padding: 7.5px 18.65px;
+    }
+    @media (max-width: 768px){
+        width: 174px;
+        height: 15px;
+        font-size: 6px;
+        padding: 3.9px 10px;
+        position: absolute;
+    }
 `;
 const SearchBut = styled.button`
     width: 45px;
@@ -69,11 +146,27 @@ const SearchBut = styled.button`
     position: absolute;
     right: 0;
     cursor: pointer;
+    @media (max-width: 1200px){
+        width: 29px;
+        height: 27px;
+    }
+    @media (max-width: 768px){
+        width: 16px;
+        height: 15px;
+    }
     &>img{
         display: block;
         margin: 0 auto;
         width: 25px;
         height: 25px;
+        @media (max-width: 1200px){
+            width: 16px;
+            height: 16px;
+        }
+        @media (max-width: 768px){
+            width: 9px;
+            height: 9px;
+        }
     }
 `;
 
